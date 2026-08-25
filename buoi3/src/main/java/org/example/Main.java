fe85338521fdee1143ffcaa498c61f3597d8468a
Nguyen Tuan Anh <ta06022006@gmail.com>
2026-08-25 15:49:29 +0700

    Delete buoi3 directory

--- buoi3/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-// Trong ứng dụng Web Java Servlet, Server Tomcat sẽ quản lý lifecycle thông qua các Servlet
-// Bạn có thể xem ví dụ tại HelloServlet.java
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dự án Web Servlet chạy trên Tomcat Server.");
-    }
-}
